aecaadf228a607a9c6a6023e3b89e105cf81e5ac
Takeshi Sugiyama <taksug229@gmail.com>
2023-09-16 00:38:56 +0900

    added config file

---
+++ src/config.py
@@ -0,0 +1,50 @@
+# -------- Basic Settings --------
+
+TARGET_F = "TARGET_BAD_FLAG"
+TARGET_A = "TARGET_LOSS_AMT"
+targetcols = [TARGET_F, TARGET_A]
+objcols = ["REASON", "JOB"]
+numcols = [
+    TARGET_F,
+    TARGET_A,
+    "LOAN",
+    "MORTDUE",
+    "VALUE",
+    "YOJ",
+    "DEROG",
+    "DELINQ",
+    "CLAGE",
+    "NINQ",
+    "CLNO",
+    "DEBTINC",
+]
+figsize = (6, 4)
+
+# -------- ML Settings --------
+random_state = 1  # Data split random_state
+cv = 5  # Cross Validation K Folds
+
+# -------- Default Parameters --------
+max_depth = 3  # Depth size for tree
+
+# -------- Grid Search Parameters --------
+UseGridSearch = False  # If True, Grid search parameters will be used. If False, Default Parameters will be used.
+
+grid_search_dic = {
+    "Decision Tree": {"max_depth": [3, 5], "min_samples_leaf": [1, 2]},
+    "Random Forest": {
+        "max_depth": [3, 5],
+        "n_estimators": [100, 200],
+        "bootstrap": [True, False],
+    },
+    "Gradient Boosting": {
+        "max_depth": [3, 5],
+        "n_estimators": [100, 200],
+        "learning_rate": [0.01, 0.1],
+    },
+}
+
+if UseGridSearch:
+    title_params = "GS-Params"
+else:
+    title_params = "Default-Params"
